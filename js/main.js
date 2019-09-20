6124d4c49aa87ded7d20c93ef8dd3904b51a6b58
AC59852 <austin.caron1@gmail.com>
2019-09-20 01:23:32 -0400

    added js functionality for about page

--- js/main.js
+++ js/main.js
@@ -1,22 +1,84 @@
-(() => {
+import Team from './modules/modules.js';
 
+(() => {
+  
     let navMobile = document.querySelector(".hmbMenu"),
         dropDown = document.querySelector(".topNav"),
         list = document.querySelector(".navList"),
-        state = false;
+        state = false,
+        carousel = document.querySelector(".carousel");
 
     function toggleMenu() {
       dropDown.classList.toggle("toggleOn");
       list.classList.toggle("show");
       if(state) {
         navMobile.src = "images/hmbrMenu.png";
+        carousel.classList.remove("hidden");
         state = false;
     }
       else{
         navMobile.src = "images/close.png";
+        carousel.classList.add("hidden");
         state = true;
     }
   }
 
-    navMobile.addEventListener("click", toggleMenu);
+  navMobile.addEventListener("click", toggleMenu);
+
+  
+let anim = document.querySelectorAll(".anim1"),
+    line4 = document.querySelector(".line3"),
+    bioData = document.querySelector(".aboutText").children,
+    bioImg = document.querySelector(".aboutPerson").children;
+
+console.log('fired!');
+
+  function parsePersonData() {
+    //this = the element you clicked on (the button)
+    let person = this.textContent;
+
+ bioData[0].textContent = Team[person].name;
+ bioData[1].textContent = Team[person].role;
+ bioData[2].textContent = Team[person].bio;
+
+ bioImg[0].src = `images/${Team[person].image}`;
+}
+
+for ( let person in Team) {
+  console.log(person);
+  let currentButton = document.createElement('div');
+  currentButton.className = 'name';
+
+  currentButton.textContent = person;
+  currentButton.addEventListener("click", parsePersonData);
+  currentButton.addEventListener("click", animation);
+
+  document.querySelector('.carousel').appendChild(currentButton);
+ };
+    
+var i;  
+function animation() {
+for (i = 0; i < anim.length; i++) {
+  anim[i].classList.add("animation");
+  anim[i].classList.remove("hidden");
+  anim[i].classList.remove("animation2");
+  anim[i].classList.remove("removeDelay");
+  };
+};
+
+function animationCheck() {
+  console.log("animation started");
+}
+
+function animationCheck2() {
+  console.log("animation ended");
+  for (i = 0; i < anim.length; i++) {
+    anim[i].classList.add("animation2");
+    anim[i].classList.remove("animation");
+    anim[i].classList.add("removeDelay");
+  };
+};
+
+line4.addEventListener("animationstart", animationCheck);
+line4.addEventListener("animationend", animationCheck2);
 })();
