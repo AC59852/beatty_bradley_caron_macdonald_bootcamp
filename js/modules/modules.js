0981c19c4266630e3fa485abb72e845ea17b149d
p-bradley <p_bradley@fanshaweonline.ca>
2019-09-20 01:33:10 -0400

    added my bio

--- js/modules/modules.js
+++ js/modules/modules.js
@@ -5,5 +5,12 @@ export default {
         bio: "Hey, my name is Austin Caron. I'm a 21 year old nerd who's ready to code your projects. I wouldn't say that I'm a designer, but I don't lack in layout, UX and UI. Along with being a nerd, my hobbies / interests include: Harry Potter, World of Warcraft, and learning JavaScript and PHP. I'm the guy that you don't often see the name of, but gets everything working smoothly (mostly).",
         image: "austin.png"
     },
+
+   Patrick: {
+        name: "Patrick Bradley",
+        role: "Front End Developer",
+        bio: "My name is Patrick Bradley, I’m  20 years old and living in London Ontario. I am mainly focused on motion design and video production, but I am increasingly becoming interested in front end coding. I am a huge fan of Tarantino movies, Nintendo games, anime and I am also a self-taught music producer (although amateur!) I have been editing photos, filming and editing videos since elementary school and combining my knowledge of film, videogames and music is a goal I set for myself every time I design a website.",
+        image: "patrick.png"
+    },
 }
 
